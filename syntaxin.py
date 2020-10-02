cd5c2f532a9fde30efcf1d184069e7dbb7a6b9c7
ARY <72200502+Ironboyind7@users.noreply.github.com>
2020-10-02 13:22:45 +0530

    Update syntaxin.py

--- syntaxin.py
+++ syntaxin.py
@@ -8,5 +8,16 @@ def to12hr(strtime):
     else:
         lis = f"{lis[0]}:{lis[1]}:{lis[2]}.AM"
         print(lis)
-        
-to12hr("12:0:0")
+def from12hrto24(strtime):
+    lis = strtime.split(":")
+    l1 = strtime.split(".")
+    if l1[-1] =="PM":
+        lis[0] = int(lis[0])+12
+        lis = lis[::-3]
+        return ":".join(lis)
+    else:
+        lis = lis[::-3]
+        return lis
+    
+print(to12hr("12:0:0"))
+print(from12hrto12("7:27:12.PM"))
